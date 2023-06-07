794aa46a65d5d7660ecf3f7d01fbba6e68fd076d
devCritical <debian09jp@gmail.com>
2023-06-07 03:46:25 -0400

    Hash and Compare Password Asynchronously

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
-const BCrypt      = require('bcrypt');
+const bcrypt      = require('bcrypt');
 fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
@@ -11,14 +11,28 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    /*Store hash in your db*/
+    /*Exapmple Console.log*/
+    console.log(hash);
 
-
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+        /*res == true or false*/
+        console.log(res);
+    });     
+});
 
 //END_ASYNC
 
 //START_SYNC
 
-
+bcrypt.hash('passw0rd!', 13, (err, hash) => {
+    console.log(hash);
+    //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
+    bcrypt.compare('passw0rd!', hash, (err, res) => {
+        console.log(res); //true
+    });
+});
 
 //END_SYNC
 

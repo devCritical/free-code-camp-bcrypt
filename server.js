eac3d71a85a9a4448a73039330d24af97d93a53e
devCritical <debian09jp@gmail.com>
2023-06-07 03:55:09 -0400

    Hash and Compare Password Synchronously

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
+const hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+const result = bcrypt.compareSync(myPlaintextPassword, hash);
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
